efd39539e165b001ef4a68a9e8c094e6ea948eb4
Leonardo Correia <128181092+Leonardo-Correia-da-silva@users.noreply.github.com>
2023-06-06 15:50:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let area_tijolo = 0.0336;
 
-let nome_pessoa = prompt('Olá, Qual o seu nome?');
+let nome_pessoa = prompt('Olá, qual o seu nome?');
            alert(`Seja bem vindo(a) ao calculador de tijolos ${nome_pessoa}, a próxima etapa é só me passar algumas informações básicas, ok?`);
 let altura_parede = prompt('Qual a altura da parade em metros?\n');
 let largura_parede = prompt('Agora me diga a largura da parede em metros? \n');
