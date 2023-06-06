4861b6e02df3138145ff1d0f27514b1e7ed6d528
Leonardo Correia <128181092+Leonardo-Correia-da-silva@users.noreply.github.com>
2023-06-06 15:40:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let area_tijolo = 0.0336;
 
 let nome_pessoa = prompt('Olá, Qual o seu nome?');
-           alert(`Seja bem vindo(a) ao calculador de tijlos ${nome_pessoa}, a próxima etapa é só me passar algumas informações básicas, ok?`);
+           alert(`Seja bem vindo(a) ao calculador de tijolos ${nome_pessoa}, a próxima etapa é só me passar algumas informações básicas, ok?`);
 let altura_parede = prompt('Qual a altura da parade em metros?\n');
-let largura_parede = prompt('Agora me diga a Largura da parede em metros? \n');
+let largura_parede = prompt('Agora me diga a largura da parede em metros? \n');
 
 let area_parede = altura_parede * largura_parede;
 let quantidade_tijolos = Math.ceil(area_parede / area_tijolo);
