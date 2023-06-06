109171d2c0ce7bb9339bab21bde3090610e8ca9e
Leonardo Correia <128181092+Leonardo-Correia-da-silva@users.noreply.github.com>
2023-06-06 13:48:14 -0300

    Brick Calculator
    This page can calculate the how much brick's in your construction

---
+++ script.js
@@ -0,0 +1,13 @@
+let area_tijolo = 0.0336;
+
+let nome_pessoa = prompt('OLÁ, QUAL SEU NOME?');
+           alert(`SEJA BEM VINDO(A) AO CALCULADOR DE TIJOLOS ${nome_pessoa}, A PRÓXIMA ETAPA É SÓ ME PASSAR ALGUMAS INFORMAÇÕES BÁSICAS`);
+let altura_parede = prompt('QUAL A ALTURA DA PAREDE?\n');
+let largura_parede = prompt('AGORA ME DIGA A LARGURA DA PAREDE?\n');
+
+let area_parede = altura_parede * largura_parede;
+let quantidade_tijolos = Math.ceil(area_parede / area_tijolo);
+
+document.write('A quantidade de tijolos que você precisa para construir essa parede é de:', quantidade_tijolos);
+
+
